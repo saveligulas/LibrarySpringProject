af1f72cc22413413d0e0f121c932e33263dfac2b
saveligulas <saveli.gulas@gmail.com>
2023-02-21 08:39:56 +0100

    added User relevant classes

---
+++ src/main/java/gulas/saveli/finalLibrary/user/Role.java
@@ -0,0 +1,16 @@
+package gulas.saveli.finalLibrary.user;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public enum Role {
+    USER(new Authority[]{Authority.USER_AUTHORITIES}), ADMIN(new Authority[]{Authority.USER_AUTHORITIES, Authority.EDIT_USERS});
+
+    private final List<Authority> AUTHORITY_LIST;
+
+    Role(Authority[] AUTHORITY_ARRAY) {
+        this.AUTHORITY_LIST = List.of(AUTHORITY_ARRAY);
+    }
+}
