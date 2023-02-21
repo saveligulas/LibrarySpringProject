dec7cf5e9c98ad892db4229c409eead26bf50a13
saveligulas <saveli.gulas@gmail.com>
2023-02-21 08:33:27 +0100

    Initial commit

---
+++ src/main/java/gulas/saveli/finalLibrary/FinalLibraryApplication.java
@@ -0,0 +1,13 @@
+package gulas.saveli.finalLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalLibraryApplication.class, args);
+	}
+
+}
