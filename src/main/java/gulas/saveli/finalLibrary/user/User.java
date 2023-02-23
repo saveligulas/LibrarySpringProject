bb51b4499c716b2f105f3829d0b6f8ec3aa3c637
saveligulas <saveli.gulas@gmail.com>
2023-02-23 14:20:52 +0100

    added new Constructor

--- src/main/java/gulas/saveli/finalLibrary/user/User.java
+++ src/main/java/gulas/saveli/finalLibrary/user/User.java
@@ -29,6 +29,14 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    public User(String firstName, String lastName, String email, String password, Role role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         List<SimpleGrantedAuthority> list = new ArrayList<>();
