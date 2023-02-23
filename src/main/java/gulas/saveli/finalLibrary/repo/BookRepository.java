cfca3c9c8c7d7e7528e3e50eacd7f75f9e801fa2
saveligulas <saveli.gulas@gmail.com>
2023-02-23 11:23:53 +0100

    added queries

--- src/main/java/gulas/saveli/finalLibrary/repo/BookRepository.java
+++ src/main/java/gulas/saveli/finalLibrary/repo/BookRepository.java
@@ -5,10 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
     @Query("SELECT b FROM Book b WHERE b.name = ?1")
     Optional<Book> findBookByName(String name);
+
+    public List<Book> findByAvailableTrue();
+
+    public List<Book> findByAvailableFalse();
+
 }
