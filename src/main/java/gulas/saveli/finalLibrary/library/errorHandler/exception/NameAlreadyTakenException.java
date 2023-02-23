cdfd7bcaca5fb6bff8a561c54b09e6157f5ed095
saveligulas <saveli.gulas@gmail.com>
2023-02-23 19:46:40 +0100

    added new Exception

---
+++ src/main/java/gulas/saveli/finalLibrary/library/errorHandler/exception/NameAlreadyTakenException.java
@@ -0,0 +1,7 @@
+package gulas.saveli.finalLibrary.library.errorHandler.exception;
+
+public class NameAlreadyTakenException extends Exception{
+    public NameAlreadyTakenException(String errorMessage) {
+        super(errorMessage);
+    }
+}
