926e2c2eefcb36b5fc47c4d2cb75ae5b29acef7d
saveligulas <saveli.gulas@gmail.com>
2023-02-28 16:22:00 +0100

    disabled cors

--- src/main/java/gulas/saveli/finalLibrary/security/config/SecurityConfiguration.java
+++ src/main/java/gulas/saveli/finalLibrary/security/config/SecurityConfiguration.java
@@ -23,6 +23,8 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors()
+                .disable()
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
