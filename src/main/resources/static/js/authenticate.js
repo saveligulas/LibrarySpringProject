7dd50ae33c4918394bd004e937aab83c7452fa12
saveligulas <saveli.gulas@gmail.com>
2023-03-02 19:18:47 +0100

    added button const and prepare params function

--- src/main/resources/static/js/authenticate.js
+++ src/main/resources/static/js/authenticate.js
@@ -1,2 +1,9 @@
 const emailHolder = document.querySelector('#email');
 const passwordHolder = document.querySelector('#password');
+
+const submitButton = document.querySelector('#cta');
+
+const prepareParams = params => ({
+    email: params.email,
+    password: params.password
+});
