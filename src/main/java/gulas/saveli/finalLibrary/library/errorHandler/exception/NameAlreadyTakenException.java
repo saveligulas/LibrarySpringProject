c066911d3146e874dc488c4358216de7449c7302
saveligulas <saveli.gulas@gmail.com>
2023-03-04 13:31:45 +0100

    deleted

--- src/main/java/gulas/saveli/finalLibrary/library/errorHandler/exception/NameAlreadyTakenException.java
+++
@@ -1,7 +0,0 @@
-package gulas.saveli.finalLibrary.library.errorHandler.exception;
-
-public class NameAlreadyTakenException extends Exception{
-    public NameAlreadyTakenException(String errorMessage) {
-        super(errorMessage);
-    }
-}
