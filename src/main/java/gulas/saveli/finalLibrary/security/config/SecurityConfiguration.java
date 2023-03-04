a1b300b12b2a71699c9671ac7747d893c431676e
saveligulas <saveli.gulas@gmail.com>
2023-03-04 14:45:45 +0100

    set allowed origin for cors

--- src/main/java/gulas/saveli/finalLibrary/security/config/SecurityConfiguration.java
+++ src/main/java/gulas/saveli/finalLibrary/security/config/SecurityConfiguration.java
@@ -58,7 +58,7 @@ public class SecurityConfiguration {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Content-Type", "application/json"));
 
