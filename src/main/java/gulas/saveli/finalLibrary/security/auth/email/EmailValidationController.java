e8adcf1c8db1ac3938466c34e2d7b8a68d1958dd
saveligulas <saveli.gulas@gmail.com>
2023-03-05 20:35:45 +0100

    code cleanup

--- src/main/java/gulas/saveli/finalLibrary/security/auth/email/EmailValidationController.java
+++ src/main/java/gulas/saveli/finalLibrary/security/auth/email/EmailValidationController.java
@@ -1,6 +1,5 @@
 package gulas.saveli.finalLibrary.security.auth.email;
 
-import gulas.saveli.finalLibrary.security.auth.RegisterRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
