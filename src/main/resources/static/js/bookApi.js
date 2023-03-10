2da68c7d709d3000444df093637e33f31212045b
saveligulas <saveli.gulas@gmail.com>
2023-03-10 16:11:28 +0100

    added fetch

--- src/main/resources/static/js/bookApi.js
+++ src/main/resources/static/js/bookApi.js
@@ -3,3 +3,15 @@ const jwtCookie = document.cookie
   .find(row => row.startsWith('Authorization='));
 
 const jwtToken = jwtCookie ? jwtCookie.split('=')[1] : null;
+
+console.log(jwtToken);
+
+fetch('http://localhost:8080/api/book', {
+    method: 'GET',
+    headers: {
+        'Authorization': `${jwtToken}`
+    }
+  })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
